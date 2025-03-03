aa4df8464162b984fb606c6affea687e07ffc54c
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2025-03-03 00:15:07 +0000

    createPortal added

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
+import { createPortal } from "react-dom";
 import "./Section.css";
 import { Modal } from "../Modal/Modal";
 
 export function Section() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const modal = createPortal(
+    <Modal onClose={() => setIsModalOpen(false)} />,
+    document.body
+  );
 
   return (
     <section>
       <h2>Nie czekaj!</h2>
       <h3>Otwórz modal!</h3>
       <button onClick={() => setIsModalOpen(true)}>Otwórz</button>
-      {isModalOpen && <Modal onClose={() => setIsModalOpen(false)} />}
+      {isModalOpen && modal}
     </section>
   );
 }
